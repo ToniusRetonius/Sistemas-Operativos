87fc7de59c7c2e0f75faff760861a70f8b3cb0f7
ToniusRetonius <toto.melli074@gmail.com>
2024-08-17 14:39:21 -0300

    P0

---
+++ Guías/P0 - bash scritping y Makefile/Resuelto/ej12.c
@@ -0,0 +1,104 @@
+// Ej 12 - plataformaCargaUsuarios
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>	
+
+typedef struct usuario_s {
+    char* nombre;    
+    int8_t edad;     
+    int32_t cuit;    
+} usuario_t;
+
+// necesitamos una función que cargue los datos en un archivo para poder escribirlo y leerlo
+void escribirArchivo(usuario_t* user){
+    FILE* archivo = fopen("ej12.txt", "w");
+    fprintf(archivo, "%s %hhd %d \n", user->nombre, user->edad, user->cuit);
+    fclose(archivo);
+}
+
+usuario_t* leerArchivo(){
+    FILE* archivo = fopen("ej12.txt", "r");
+
+    usuario_t* lectura = malloc(sizeof(usuario_t));
+    lectura->nombre = malloc(25 * sizeof(char));
+
+    // https://www.w3schools.com/c/c_files_read.php
+    char linea[256];
+    fgets(linea, sizeof(linea), archivo);
+    
+    // sscanf se usa cuando tienes una cadena de texto con datos y deseas extraer esos datos en variables separadas. Es útil para procesar entradas que tienen un formato específico.
+    sscanf(linea, "%24s %hhd %d", lectura->nombre, &lectura->edad, &lectura->cuit);
+
+    fclose(archivo);
+    return lectura;
+}
+
+int main(int argc, char *argv[])
+{
+    // ingresan 1 argumento : puede ser verInformacionUsuario o agregarInformacionUsuario
+    // según el caso vamos a definir entonces
+    char* entrada = argv[1];
+    char* verInfo = "verInformacionUsuario";
+    char* agregarInfo = "agregarInformacionUsuario";
+
+
+    if ((strcmp(entrada, verInfo)) == 0)
+    {
+        usuario_t* usuario = leerArchivo();
+        if (usuario == NULL)
+        {
+            printf("¡Ningún usuario fue cargado a la plataforma todavía!\n");
+        } else
+        {
+            printf("Nombre: %s ,", usuario->nombre);
+            printf("edad: %hhd ,", usuario->edad);
+            printf("Nombre: %d \n", usuario->cuit);
+        }
+
+    } else if ((strcmp(entrada, agregarInfo)) == 0)
+    {   
+        // pedimos memoria
+        usuario_t* user = malloc(sizeof(usuario_t));
+        user->nombre = malloc(25 * sizeof(char));
+
+        // Ingrese nombre de persona:
+        printf("Ingrese nombre de persona:");
+        scanf("%24s", user->nombre);
+
+        // Ingrese edad:
+        int8_t edad;
+        printf("Ingrese edad:");
+        scanf("%hhd", &edad);
+        if (edad > 18 && edad < 99)
+        {
+            user->edad = edad;
+        }else
+        {
+            printf("La información cargada es invalida, por favor, intente de nuevo\n");
+            return 0;
+        }
+        
+        int32_t cuit;
+        // Ingrese cuit:
+        printf("Ingrese cuit:");
+        scanf("%d", &cuit);
+        user->cuit = cuit;
+        
+        // tenemos que chequear el ej anterior para que funcione y luego implementarlo acǽ
+        escribirArchivo(user);
+        // ¡Su información fue cargada con éxito!
+        printf("¡Su información fue cargada con éxito!\n");
+    }
+    else
+    {
+        printf("Poné una opción válida culeado! \n");
+    }
+    
+    return 0;
+}
+
+// falla la escritura del cuit . me pone 0
+// falla la lectura 
+
+
